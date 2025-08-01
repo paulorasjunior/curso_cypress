33a4ab63883dcb813a4d55fd49a27d34ce312e93
PAULO JR <paulorasjunior@gmail.com>
2025-08-01 10:46:38 -0300

    Atualização do arquivo config.js para forçar abrir com chrome

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,9 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return config;
     },
+    chromeWebSecurity: false,
     video: true,
     reporter: 'mochawesome',
     reporterOptions: {
